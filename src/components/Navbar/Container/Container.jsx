bbdbeb207dd386b19d581a0a493bedfc87964816
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-13 04:47:07 +0600

    Navbar

---
+++ src/components/Navbar/Container/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Container = ({className,children}) => {
+  return (
+    <div className={`max-w-[1170px] mx-auto ${className}`}>
+        {
+            children
+        }
+      
+    </div>
+  )
+}
+
+export default Container
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from './Container/Container'
+
+const Navbar = () => {
+  return (
+
+    <nav>
+        <Container>
+            <h1>
+                ghjkl
+            </h1>
+        </Container>
+      
+    </nav>
+  )
+}
+
+export default Navbar
