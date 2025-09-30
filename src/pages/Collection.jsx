635344832b9108845dccdf83940ae7cbad20effc
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-30 06:17:39 +0600

    Related Item

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title/Title"
 import ProductItem from '../components/ProductItem/ProductItem';
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products ,search ,showSearch } = useContext(ShopContext);
 
   const [showFilter, setShowFilter] = useState(false);
 
@@ -42,6 +42,10 @@ const Collection = () => {
   const applyFilter = () => {
     let productsCopy =  products.slice();
 
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if (category.length > 0 ){
       productsCopy = productsCopy.filter(item => category.includes(item.category))
     }
@@ -75,7 +79,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter()
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
