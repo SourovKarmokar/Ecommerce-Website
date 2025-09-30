2f335b89be3a07194da118e87010c84271d5e262
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-30 09:01:31 +0600

    Product

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+
+
+import { ToastContainer, toast } from 'react-toastify';
 
 
 export const ShopContext = createContext();
@@ -7,8 +10,56 @@ export const ShopContext = createContext();
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const delivery_fee = 10;
-  const [ search , setSearch ] = useState('');
-  const  [ showSearch , setShowSearch ] = useState(false);
+  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItem, setCartItem] = useState({});
+
+
+  const addToCard = async (itemId, size) => {
+
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
+
+    let cartData = structuredClone(cartItem);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      }
+      else {
+        cartData[itemId][size] = 1;
+      }
+    }
+    else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItem(cartData);
+  }
+
+
+
+
+  const getCartCount = () => {
+    let totalCount = 0 ;
+    for(const items in cartItem){
+      for(const item in cartItem[items]){
+        try{
+          if(cartItem[items][item] > 0)
+            totalCount += cartItem[items][item];
+        }catch(error){
+
+        }
+      }
+    }
+    return totalCount;
+  }
+
+
+
+
 
   const value = {
     products,
@@ -18,11 +69,27 @@ const ShopContextProvider = ({ children }) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItem,
+    addToCard,
+    getCartCount,
   };
 
   return (
     <ShopContext.Provider value={value}>
       {children}
+      {/* ✅ Toast container should be rendered here, once */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </ShopContext.Provider>
   );
 };
